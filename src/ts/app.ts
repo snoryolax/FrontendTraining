09ef35e45693b9c2fd92445c36d1830b22773040
ryo ito <r.ito@hyge.co.jp>
2021-11-09 15:41:28 +0900

    [add] ESLintで構文チェックが出来るように調整

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,3 +1,2 @@
-import getRandomElement from './animals';
-import { Animals, Dog } from './animals';
-import '../scss/style.scss';
+import getRandomElement, { Animals, Dog } from './animals'
+import '../scss/style.scss'
