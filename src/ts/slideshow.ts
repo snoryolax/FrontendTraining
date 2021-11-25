eb37962da6992e487e4d11d7efde7d8a088edbca
ryo ito <r.ito@hyge.co.jp>
2021-11-25 11:17:20 +0900

    [fix] image-containerとtext-containerを統合

--- src/ts/slideshow.ts
+++ src/ts/slideshow.ts
@@ -4,16 +4,16 @@ const MAX_IMAGE_NUM: number = 3 // スライドショーの画像の枚数
 const toggleImg = (imgCount: number) => {
   // 表示する画像が含まれる要素の取得
   const activeImageElem = document.getElementById(
-    `image0${imgCount}`
+    `slide0${imgCount}`
   ) as HTMLAnchorElement
 
   switch (imgCount) {
     case 1: {
       const inactiveImageElem01 = document.getElementById(
-        `image0${imgCount + 1}`
+        `slide0${imgCount + 1}`
       ) as HTMLAnchorElement
       const inactiveImageElem02 = document.getElementById(
-        `image0${imgCount + 2}`
+        `slide0${imgCount + 2}`
       ) as HTMLAnchorElement
       inactiveImageElem01.style.display = 'none'
       inactiveImageElem02.style.display = 'none'
@@ -22,10 +22,10 @@ const toggleImg = (imgCount: number) => {
     }
     case 2: {
       const inactiveImageElem01 = document.getElementById(
-        `image0${imgCount + 1}`
+        `slide0${imgCount + 1}`
       ) as HTMLAnchorElement
       const inactiveImageElem02 = document.getElementById(
-        `image0${imgCount - 1}`
+        `slide0${imgCount - 1}`
       ) as HTMLAnchorElement
       inactiveImageElem01.style.display = 'none'
       inactiveImageElem02.style.display = 'none'
@@ -34,10 +34,10 @@ const toggleImg = (imgCount: number) => {
     }
     case 3: {
       const inactiveImageElem01 = document.getElementById(
-        `image0${imgCount - 1}`
+        `slide0${imgCount - 1}`
       ) as HTMLAnchorElement
       const inactiveImageElem02 = document.getElementById(
-        `image0${imgCount - 2}`
+        `slide0${imgCount - 2}`
       ) as HTMLAnchorElement
       inactiveImageElem01.style.display = 'none'
       inactiveImageElem02.style.display = 'none'
@@ -55,62 +55,11 @@ const changeImage = (count: number) => {
   toggleImg(imgCount)
 }
 
-// スライドショーのテキストを切り替える
-const changeTitle = (count: number) => {
-  const titleCount: number = count
-
-  // 表示するテキストが含まれる要素の取得
-  const activeTitleElem = document.getElementById(
-    `title0${titleCount}`
-  ) as HTMLAnchorElement
-
-  // titleCountによる画像の出し分け
-  switch (titleCount) {
-    case 1: {
-      const inactiveTitleElem01 = document.getElementById(
-        `title0${titleCount + 1}`
-      ) as HTMLAnchorElement
-      const inactiveTitleElem02 = document.getElementById(
-        `title0${titleCount + 2}`
-      ) as HTMLAnchorElement
-      inactiveTitleElem01.style.display = 'none'
-      inactiveTitleElem02.style.display = 'none'
-      activeTitleElem.style.display = 'block'
-      break
-    }
-    case 2: {
-      const inactiveTitleElem01 = document.getElementById(
-        `title0${titleCount + 1}`
-      ) as HTMLAnchorElement
-      const inactiveTitleElem02 = document.getElementById(
-        `title0${titleCount - 1}`
-      ) as HTMLAnchorElement
-      inactiveTitleElem01.style.display = 'none'
-      inactiveTitleElem02.style.display = 'none'
-      activeTitleElem.style.display = 'block'
-      break
-    }
-    case 3: {
-      const inactiveTitleElem01 = document.getElementById(
-        `title0${titleCount - 1}`
-      ) as HTMLAnchorElement
-      const inactiveTitleElem02 = document.getElementById(
-        `title0${titleCount - 2}`
-      ) as HTMLAnchorElement
-      inactiveTitleElem01.style.display = 'none'
-      inactiveTitleElem02.style.display = 'none'
-      activeTitleElem.style.display = 'block'
-      break
-    }
-  }
-}
-
 const slideshow = (interval: number) => {
   let count: number = 1 // 2→3→1→2→3→1...
   setInterval(() => {
     count++
     changeImage(count)
-    changeTitle(count)
     if (count === MAX_IMAGE_NUM) {
       count = 0
     }
