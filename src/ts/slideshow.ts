cbaade294e4bc0d0d2d22ebef578890a0ec93a2f
ryo ito <r.ito@hyge.co.jp>
2021-12-03 11:53:55 +0900

    [fix] スライドの切り替えの処理を単純化

--- src/ts/slideshow.ts
+++ src/ts/slideshow.ts
@@ -1,8 +1,8 @@
-// スライドの枚数
+/* スライドの枚数 */
 const SLIDE_NUM: number =
   document.getElementsByClassName('hero-container').length
 
-// スライドの要素を取得
+/* スライドの要素を取得 */
 const loadSlideElem = () => {
   const slideArray: HTMLDivElement[] = []
   for (let i = 0; i < SLIDE_NUM; i++) {
@@ -11,25 +11,27 @@ const loadSlideElem = () => {
   return slideArray
 }
 
-// スライドを切り替え
+/* スライドを切り替え */
 const toggleSlide = (slideCount: number, slideElems: HTMLDivElement[]) => {
-  const index = slideCount - 1 // 表示するスライドのインデックス
+  // 表示するスライドのインデックス
+  const index = slideCount - 1
 
-  // 表示するスライド
-  slideElems[index].style.display = 'block'
-
-  // 表示しないスライド
+  // すべてのスライドを非表示
   for (let i = 0; i < SLIDE_NUM; i++) {
-    if (index !== i) {
-      slideElems[i].style.display = 'none'
-    }
+    slideElems[i].style.display = 'none'
   }
+
+  // 特定のスライドのみを表示
+  slideElems[index].style.display = 'block'
 }
 
-// スライドショーの実行
+/* スライドショーの実行 */
 const slideshow = (interval: number) => {
-  let slideCount: number = 1 // 2→3→1→2→3→1...
+  // 2→3→1→2→3→1...
+  let slideCount: number = 1
+  // スライドの要素を取得
   const slideElems: HTMLDivElement[] = loadSlideElem()
+
   setInterval(() => {
     slideCount++
     toggleSlide(slideCount, slideElems)
