7c85c66ecf0685921b221d4ba63091e6baab3460
ryo ito <r.ito@hyge.co.jp>
2021-12-06 18:18:05 +0900

    [fix] メニューオープン時のクラス名を変更

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -6,17 +6,17 @@ const header = () => {
 
     // dotの背景の円の表示切り替え
     const circle = document.querySelector('.circle')
-    circle!.classList.toggle('is-open')
+    circle!.classList.toggle('menu-active')
 
     // dotの表示切り替え
     const dot = document.querySelectorAll('.dot')
     for (let i = 0; i < dot.length; i++) {
-      dot[i].classList.toggle('is-open')
+      dot[i].classList.toggle('menu-active')
     }
 
     // 文字色の切り替え
     const title = document.querySelector('.header-container')
-    title!.classList.toggle('is-open')
+    title!.classList.toggle('menu-active')
   }
 }
 
